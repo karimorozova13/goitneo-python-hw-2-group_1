38441f7b34e18c2c5f5b604dae64f385c64fefe8
karimorozova13 <k.morozova@tiomarkets.com>
2023-12-05 16:03:25 +0200

    first task

--- cli_bot.py
+++ cli_bot.py
@@ -4,30 +4,69 @@ def parse_input(user_input):
     return cmd, *args
 
 
-def change_contact(args,contacts):
-    name,phone = args
-    contacts.update({name:phone})
-    return "Contact updated"
+# def change_contact(args,contacts):
+#     name,phone = args
+#     contacts.update({name:phone})
+#     return "Contact updated"
 
 def input_error(fn):
     def inner(*args, **kwargs):
+        
         try:
             return fn(args, kwargs)
         except ValueError:
-            "Give me name and phone please"
+            print("Not enough values to unpack. Give me name and phone please")
+            return kwargs
         except TypeError:
-            "Enter correct type"
+            print("Enter correct type")
+            return kwargs
+        except NameError:
+            print('Such contact does not exist')
+            return kwargs
+        except IndexError:
+            print("We do not have this contact or Not enough values to unpack.")
+            return kwargs
+             
     return inner
 
 @input_error
 def add_contact(args,contacts):
-    name,phone = args
-    contacts[name] =phone
-    return "Contact added"
+    name= args[0][0]
+    phone= args[0][1]
+    
+    if phone and name:
+        contacts[name] =phone
+        print("Contact added")
+        return contacts
+    else:
+        raise IndexError
 
+@input_error
+def change_contact(args,contacts):
+    name= args[0][0]
+    phone= args[0][1]
+    
+    if name in contacts.keys():
+        contacts.update({name:phone})
+        print('Contact was updated')
+        return contacts
+    else:
+        raise NameError
 
+@input_error
+def show_all(args,contacts):
+    print(contacts)
+    return contacts
 
-
+@input_error
+def show_phone(args, contacts):
+    name= args[0][0]
+    if name in contacts.keys():
+        return contacts[name]
+    else:
+        raise NameError
+    
+    
 def main():
     print('Welcome to the assistant bot!')
     contacts = {}
@@ -40,13 +79,13 @@ def main():
         elif command=='hello':
             print('How can I help you?')
         elif command == 'add':
-            print(add_contact(args, contacts))
+            contacts = add_contact(args, **contacts)
         elif command == 'change':
-            print(change_contact(args, contacts))
+            contacts = change_contact(args, **contacts)
         elif command == 'all':
-            print(contacts)
-        elif command.isdecimal():
-            print(contacts[args[0]])
+            print(show_all(**contacts))
+        elif command =='phone':
+            print(show_phone(args, **contacts))
         else:
             print('Invalid command')
 
